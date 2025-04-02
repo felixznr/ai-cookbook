d8dfc42c1a0391c45f439adc9605bd9381361388
felixznr <felixzauner21@icloud.com>
2025-04-02 08:17:48 +0200

    Implement weather assistant functionality with OpenAI integration

--- personal/tools.py
+++ personal/tools.py
@@ -22,4 +22,90 @@ def get_weather(latitude, longitude):
     data = response.json()
     return data["current"]
 
-print(get_weather(47.8095,13.0550))
+# Step 1 Call the wheater tools
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "Get current temperature for provided coordinates in celsius.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "latitude": {"type": "number"},
+                    "longitude": {"type": "number"},
+                },
+                "required": ["latitude", "longitude"],
+                "additionalProperties": False,
+            },
+            "strict": True,
+        },
+    }
+]
+
+system_prompt = "You are a helpful wheater assistant."
+
+messages = [
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": "What's the wheater in Salzburg today."},
+]
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+)
+
+
+# 2 Model dicides to call functions
+
+completion.model_dump()
+
+
+# 3 Execute get_wheater function
+
+def call_function(name, args):
+    if name == "get_weather":
+        return get_weather(**args)
+    
+for tool_call in completion.choices[0].message.tool_calls:
+    name = tool_call.function.name
+    args = json.loads(tool_call.function.arguments)
+    messages.append(completion.choices[0].message)
+    
+    #Testen
+    print(name)
+    print(args)
+    print(messages)
+
+
+    result = call_function(name, args)
+    messages.append(
+        {"role": "tool", "tool_call_id": tool_call.id, "content": json.dumps(result)}
+    )
+
+
+
+    # 4 Supply result and call model again
+
+    class WeatherResponse(BaseModel):
+        temperature: float = Field(
+            description="The current temperatur in celsius for the given location"
+        )
+        response: str=Field(
+            description="A natrual languange response to the user's question"
+        )
+
+completion_2 = client.beta.chat.completions.parse(
+    model="gpt-4o",
+    messages=messages,
+    tools=tools,
+    response_format=WeatherResponse,
+)
+
+# 5 Check model response
+
+final_response = completion_2.choices[0].message.parsed
+print(final_response.temperature)
+print(final_response.response)
